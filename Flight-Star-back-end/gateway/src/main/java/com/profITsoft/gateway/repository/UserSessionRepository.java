53316326932f036d6ac9cd810e2455a7664d4ad6
Andrii Pyroh <andrewpyrih@gmail.com>
2024-06-25 11:07:47 +0300

    Switched gateway to use redis instead of h2 inmemory db

--- Flight-Star-back-end/gateway/src/main/java/com/profITsoft/gateway/repository/UserSessionRepository.java
+++ Flight-Star-back-end/gateway/src/main/java/com/profITsoft/gateway/repository/UserSessionRepository.java
@@ -2,36 +2,13 @@ package com.profITsoft.gateway.repository;
 
 import com.profITsoft.gateway.auth.dto.UserInfo;
 import com.profITsoft.gateway.data.UserSession;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
-import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
 import java.time.Instant;
-import java.util.UUID;
 
-import static org.springframework.data.relational.core.query.Criteria.where;
-import static org.springframework.data.relational.core.query.Query.query;
+public interface UserSessionRepository {
 
-@Repository
-@RequiredArgsConstructor
-public class UserSessionRepository {
+    Mono<UserSession> createSession(UserInfo userInfo, Instant expiresAt);
 
-  private final R2dbcEntityTemplate entityTemplate;
-
-  public Mono<UserSession> createSession(UserInfo userInfo, Instant expiresAt) {
-    UserSession userSession = new UserSession();
-    userSession.setId(UUID.randomUUID().toString());
-    userSession.setEmail(userInfo.getEmail());
-    userSession.setName(userInfo.getName());
-    userSession.setExpiresAt(expiresAt);
-    return entityTemplate.insert(userSession);
-  }
-
-  public Mono<UserSession> findById(String id) {
-    return entityTemplate.selectOne(
-        query(where("id").is(id)),
-        UserSession.class);
-  }
-
+    Mono<UserSession> findById(String id);
 }
